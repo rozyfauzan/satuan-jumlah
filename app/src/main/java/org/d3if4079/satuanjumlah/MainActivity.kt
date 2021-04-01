519184a2b803e2f4d693ace759210dbbab05d43d
rozyfauzan <rozyfauzan28@gmail.com>
2021-04-01 21:59:25 +0700

    first commit

---
+++ app/src/main/java/org/d3if4079/satuanjumlah/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if4079.satuanjumlah
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
